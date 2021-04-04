29dd1a336795c8ff7bd09a77888fd65c045f1d0a
Nic <nicholasbshepard@github.ccs.neu.edu>
2021-04-04 10:02:26 -0400

    Ref of user loading

--- app/src/main/java/edu/neu/madcourseworkupteam/workup/MainActivity.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/MainActivity.java
@@ -2,11 +2,17 @@ package edu.neu.madcourseworkupteam.workup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "WorkUp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
 
         db.child("users").child("1").setValue(user);
 
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get User object and use the values to update the UI
+                User dbuser = dataSnapshot.getValue(User.class);
+                Log.w(TAG, dbuser.toString());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+
+        db.addValueEventListener(userListener);
     }
 }
