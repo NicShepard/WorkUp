fd1a26c60fcc68b6dad8b40bf0cff56d4966346a
nberg002 <berg.n@northeastern.edu>
2021-04-05 11:05:18 -0400

    Login Screen UI design

---
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/ExploreScreenActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourseworkupteam.workup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_screen);
+    }
+}
