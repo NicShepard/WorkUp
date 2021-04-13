302118dacfbbb089529fc12d2cb3f0a4793b503a
Nic <nicholasbshepard@github.ccs.neu.edu>
2021-04-13 18:33:52 -0400

    Sensor detected

--- app/src/main/java/edu/neu/madcourseworkupteam/workup/Registration.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/Registration.java
@@ -2,6 +2,10 @@ package edu.neu.madcourseworkupteam.workup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,7 +17,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class Registration extends AppCompatActivity {
+public class Registration extends AppCompatActivity implements SensorEventListener {
 
     Button testButton;
     private Button testButton2;
@@ -23,13 +27,15 @@ public class Registration extends AppCompatActivity {
     String uid;
     Movement m;
 
+    SensorManager sensorManager;
+    Sensor stepCounter;
+
     DataService dataService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
         dataService = new DataService();
 
         uid = getIntent().getStringExtra("CURRENT_USER");
@@ -37,6 +43,17 @@ public class Registration extends AppCompatActivity {
         fName = findViewById(R.id.firstName);
         lName = findViewById(R.id.lastName);
         username = findViewById(R.id.username);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
+            stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            fName.setText("Step counter detected");
+        } else {
+            fName.setText("No Step counter detected");
+
+        }
+
 
         testButton2 = findViewById(R.id.getUser);
         testButton.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +70,37 @@ public class Registration extends AppCompatActivity {
                 Log.d("Movement", m.getTitle());
             }
         });
+
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this, stepCounter);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
