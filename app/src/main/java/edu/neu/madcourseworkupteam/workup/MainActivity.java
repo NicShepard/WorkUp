8fbd087fdab5527dc6838b6f6b43f03d10508da1
ziam <ziam@husky.neu.edu>
2021-04-14 02:24:38 -0400

    move ring updates

--- app/src/main/java/edu/neu/madcourseworkupteam/workup/MainActivity.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/MainActivity.java
@@ -1,13 +1,23 @@
 package edu.neu.madcourseworkupteam.workup;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.moveRing);
+        Intent move_ring = new Intent(this, MoveRing.class);
+        startActivity(move_ring);
+
+
     }
 }
--- app/src/main/java/edu/neu/madcourseworkupteam/workup/MoveRing.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/MoveRing.java
@@ -14,19 +14,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MoveRing extends AppCompatActivity implements SensorEventListener, StepListener {
-
     private SensorManager sensorManager;
     private boolean running;
     private int totalSteps;
     private int previousSteps;
-    private Sensor accel;
-    private TextView stepCount;
-    private Sensor stepSensor;
-    private static final String NUM_STEPS = "";
+    private TextView stepsTxtView;
+
+
 
 
 
@@ -37,50 +37,76 @@ public class MoveRing extends AppCompatActivity implements SensorEventListener, 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        running = false;
+        totalSteps = 0;
+        previousSteps = 0;
+        stepsTxtView = findViewById(R.id.steps);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        stepCount = findViewById(R.id.steps);
-        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
-        if (stepSensor == null) {
-            Toast.makeText(this, "No sensor detected", Toast.LENGTH_SHORT).show();
-        } else{
-            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
-        }
-
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+
+//        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+//        stepCount = findViewById(R.id.steps);
+//        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+//
+//        if (stepSensor == null) {
+//            Toast.makeText(this, "No sensor detected", Toast.LENGTH_SHORT).show();
+//        } else{
+//            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
+//        }
+//
+//    }
 
     @Override
     public void onResume() {
         super.onResume();
         running = true;
 
-        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+        Sensor stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
 
+        if(stepSensor == null) {
+            Toast.makeText(this, "No sensor detected", Toast.LENGTH_SHORT).show();
+        } else{
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_GAME);
+        }
+    }
+
+    @Override public void onPause() {
+        super.onPause();
+
+
+//        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+//            sensorManager.unregisterListener(this, stepSensor);
+//        }
     }
 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+
+        if(running) {
+            totalSteps = (int) event.values[0];
+            stepsTxtView.setText(String.valueOf(totalSteps));
+        }
+
+
 //        if(running) {
 //            totalSteps = event.values[0] }
 
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                stepSensor.updateAccel(event.timestamp, event.values[0], event.values[1], event.values[2]);
+//            if (event.sensor == stepSensor) {
+//                totalSteps = (int)event.values[0];
+//                stepCount.setText(String.valueOf(totalSteps) );
+
+
+//                totalSteps = (int)event.values[0];
+
+                //updateAccel(event.timestamp, event.values[0], event.values[1], event.values[2]);
 
         }
 
-    }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -89,6 +115,11 @@ public class MoveRing extends AppCompatActivity implements SensorEventListener, 
 
     @Override
     public void step(long timeNs) {
+
+    }
+
+    //TODO reset steps at the end of day?
+    private void resetSteps() {
 
     }
 }
