7cabd72c76ba1bfc1ae814ae4fbf9a8f97e96486
Nic <nicholasbshepard@github.ccs.neu.edu>
2021-04-19 20:54:57 -0400

    Step counter refactored, tracking steps now

--- app/src/main/java/edu/neu/madcourseworkupteam/workup/MainActivity.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/MainActivity.java
@@ -5,28 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -41,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, VideoCard.class);
+        startActivity(intent);
+
 
         mEmail = findViewById(R.id.UserName);
         mPassword = findViewById(R.id.Password);
--- app/src/main/java/edu/neu/madcourseworkupteam/workup/VideoCard.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/VideoCard.java
@@ -6,28 +6,49 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class StepCounter extends AppCompatActivity implements SensorEventListener {
+public class VideoCard extends AppCompatActivity implements SensorEventListener {
 
     int steps;
     SensorManager sensorManager;
     Sensor stepCounter;
-    TextView pedometer;
+    TextView stepDisplay;
+    Button startCounter;
+    Boolean active;
+    VideoCard sc = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_step_counter);
+        setContentView(R.layout.video_card);
+        active = false;
 
         steps = 0;
-        pedometer = findViewById(R.id.pedometerDisplay);
+        stepDisplay = findViewById(R.id.step_display);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        startCounter = findViewById(R.id.startPedometer);
 
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
-            stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-            sensorManager.registerListener(this, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
-        }
+        startCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(active){
+                    startCounter.setText("Start Pedometer");
+                    sensorManager.unregisterListener(sc, stepCounter);
+                    active = false;
+                } else {
+                    startCounter.setText("Stop Pedometer");
+                    if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null){
+                        stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+                        sensorManager.registerListener(sc, stepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+                    }
+                    active = true;
+                }
+            }
+        });
     }
 
     @Override
@@ -48,7 +69,7 @@ public class StepCounter extends AppCompatActivity implements SensorEventListene
     public void onSensorChanged(SensorEvent event) {
         if(event.sensor == stepCounter){
             steps = (int) event.values[0];
-            pedometer.setText(String.valueOf(steps));
+            stepDisplay.setText(String.valueOf(steps));
         }
     }
 
