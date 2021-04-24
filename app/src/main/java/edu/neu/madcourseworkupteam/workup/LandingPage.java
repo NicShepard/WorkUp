623ec541f70c94160f03cf5653d8ee87256ffc30
ziam <ziam@husky.neu.edu>
2021-04-24 15:50:18 -0400

    Drawer connected to profile and explore; profile pic shows

--- app/src/main/java/edu/neu/madcourseworkupteam/workup/LandingPage.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/LandingPage.java
@@ -19,6 +19,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -51,6 +52,9 @@ public class LandingPage extends AppCompatActivity {
 
     //private DatabaseReference database;
 
+
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,6 +71,22 @@ public class LandingPage extends AppCompatActivity {
         bottomNavigation = findViewById(R.id.bottom_navigation);
 
         bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.goProfile:
+                        goToProfile();
+                        break;
+                    case R.id.gotToExplore:
+                        goToExplore();
+                        break;
+                }
+                return true;
+            }
+        });
 
 
         mFirebaseDB = FirebaseDatabase.getInstance().getReference();
@@ -167,6 +187,18 @@ public class LandingPage extends AppCompatActivity {
 
         challengeAdapter.notifyItemInserted(position);
         return 1;
+    }
+
+    private void goToProfile(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void goToExplore(){
+        Intent intent = new Intent(this, ExploreScreenActivity.class);
+        startActivity(intent);
+
     }
 
     @Override
