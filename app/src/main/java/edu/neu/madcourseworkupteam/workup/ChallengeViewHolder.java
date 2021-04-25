0d77c3f0680a2c2623148c44fe16d0848a0ecf0d
nberg002 <berg.n@northeastern.edu>
2021-04-24 20:07:49 -0400

    create new challenge view from click and display data

--- app/src/main/java/edu/neu/madcourseworkupteam/workup/ChallengeViewHolder.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/workup/ChallengeViewHolder.java
@@ -1,12 +1,19 @@
 package edu.neu.madcourseworkupteam.workup;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Date;
 
@@ -15,6 +22,7 @@ public class ChallengeViewHolder extends RecyclerView.ViewHolder {
     public TextView dates;
     public TextView challengeName;
     public TextView friendNames;
+    public String challengeID;
 
 
     public ChallengeViewHolder(@NonNull View itemView, final ItemClickListener listener) {
@@ -31,13 +39,26 @@ public class ChallengeViewHolder extends RecyclerView.ViewHolder {
                     int position = getLayoutPosition();
                     listener.onItemClick(position);
 
-                    Intent intent = new Intent(itemView.getContext(), ChallengeView.class);
-                    intent.putExtra("videoURL", "");
-                    intent.putExtra("videoTitle", "");
-                    intent.putExtra("videoDesc", "");
+                    ValueEventListener userListener = new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Log.d("Get challenge on data change!!!", "called");
+                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                Log.d("Get challenge key", String.valueOf(ds.getKey()));
+                                Intent intent = new Intent(itemView.getContext(), ChallengeView.class);
+                                intent.putExtra("challengeID", String.valueOf(ds.getKey()));
+                                itemView.getContext().startActivity(intent);
+                            }
+                        }
 
-                    itemView.getContext().startActivity(intent);
-
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            // Getting Post failed, log a message
+                            Log.w("Get Movement", "loadPost:onCancelled", databaseError.toException());
+                        }
+                    };
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("challenges");
+                    databaseReference.addValueEventListener(userListener);
                 }
             }
         });
